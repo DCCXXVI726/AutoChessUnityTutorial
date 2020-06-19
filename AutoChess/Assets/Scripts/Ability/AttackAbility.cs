ce2aa6de72184d9e801faba328762673206dc025
DCCXXVI726 <Dccxxvi726726@gmail.com>
2020-06-19 17:27:03 +0300

    Start AttackCommad

---
+++ AutoChess/Assets/Scripts/Ability/AttackAbility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAbility : Ability
+{
+    public float dmg;
+    public float delayAttack;
+    public float lastAttack;
+    public float rangeAttack;
+}
---
+++ AutoChess/Assets/Scripts/Commands/AttackCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCommand : Command
+{
+    Character character;
+    Character target;
+
+    public AttackCommand(Character character, Character target)
+    {
+        this.character = character;
+        this.target = target;
+        this.Execute();
+    }
+
+    public void Execute()
+    {
+        AttackAbility attackAbility = (AttackAbility)character.getAbility(typeof(AttackAbility));
+        if (attackAbility == null)
+        {
+            Debug.Log("Don't have attack ability");
+            return;
+        }
+        // добавить проверку на рендж
+        if (Time.time >= attackAbility.lastAttack + attackAbility.delayAttack)
+        {
+            new DealDmgCommand(target, attackAbility.dmg);
+        }
+    }
+}
